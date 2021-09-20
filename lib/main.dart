7eaa3cae3bc4a6602855fa903829ca74b3f8cc3c
sakushin <sakushin.udemy@gmail.com>
2021-09-21 08:57:20 +0900

    23_データクラスをStateProviderにする、までの実装

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_countup/provider.dart';
 
+import 'data/count_data.dart';
+
 void main() {
   runApp(
     ProviderScope(
@@ -53,18 +55,30 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               ref.watch(messageProvider),
             ),
             Text(
-              ref.watch(countProvider).state.toString(),
+              ref.watch(countDataProvider).state.count.toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 FloatingActionButton(
-                  onPressed: () => ref.read(countProvider).state++,
+                  onPressed: () {
+                    CountData countData = ref.read(countDataProvider).state;
+                    ref.read(countDataProvider).state = countData.copyWith(
+                      count: countData.count + 1,
+                      countUp: countData.countUp + 1,
+                    );
+                  },
                   child: const Icon(CupertinoIcons.plus),
                 ),
                 FloatingActionButton(
-                  onPressed: () => ref.read(countProvider).state++,
+                  onPressed: () {
+                    CountData countData = ref.read(countDataProvider).state;
+                    ref.read(countDataProvider).state = countData.copyWith(
+                      count: countData.count - 1,
+                      countDown: countData.countDown + 1,
+                    );
+                  },
                   child: const Icon(CupertinoIcons.minus),
                 ),
               ],
@@ -72,15 +86,25 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('1'),
-                Text('2'),
+                Text(
+                  ref.watch(countDataProvider).state.countUp.toString(),
+                ),
+                Text(
+                  ref.watch(countDataProvider).state.countDown.toString(),
+                ),
               ],
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(countProvider).state++,
+        onPressed: () {
+          ref.read(countDataProvider).state = CountData(
+            count: 0,
+            countUp: 0,
+            countDown: 0,
+          );
+        },
         child: const Icon(Icons.refresh),
       ),
     );
