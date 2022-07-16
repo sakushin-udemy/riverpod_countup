e9a30a98b4ca178cb2ca66542ececdfc2ddea004
sakushin <sakushin.udemy@gmail.com>
2022-07-17 07:46:53 +0900

    アニメーションが追加されたので、pumpAndSettleに変更した

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -21,13 +20,16 @@ void main() {
       ),
     );
 
+    await tester.tap(find.byIcon(CupertinoIcons.refresh));
+    await tester.pumpAndSettle();
+
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsNWidgets(3));
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(CupertinoIcons.plus));
-    await tester.pump();
+    await tester.pumpAndSettle();
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsOneWidget);
